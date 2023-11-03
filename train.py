4d899ed73ddd0ba4fc95cff00b8256938389996b
leying <vivian556123@icloud.com>
2023-11-03 01:57:09 -0700

    hifigan-train fisher 8k 20ms

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ def train(rank, a, h):
             if rank == 0:
                 start_b = time.time()
             x, y, _, y_mel = batch
-            print("y_mel",y_mel.shape)
+            #print("y_mel",y_mel.shape)
             x = torch.autograd.Variable(x.to(device, non_blocking=True))
             y = torch.autograd.Variable(y.to(device, non_blocking=True))
             y_mel = torch.autograd.Variable(y_mel.to(device, non_blocking=True))
@@ -133,7 +133,7 @@ def train(rank, a, h):
             y_g_hat = generator(x)
             y_g_hat_mel = mel_spectrogram(y_g_hat.squeeze(1), h.n_fft, h.num_mels, h.sampling_rate, h.hop_size, h.win_size,
                                           h.fmin, h.fmax_for_loss)
-            print("y_g_hat_mel",y_g_hat_mel.shape)
+            #print("y_g_hat_mel",y_g_hat_mel.shape)
             optim_d.zero_grad()
 
             # MPD
@@ -154,7 +154,7 @@ def train(rank, a, h):
 
             # L1 Mel-Spectrogram Loss
             loss_mel = F.l1_loss(y_mel, y_g_hat_mel) * 45
-            print("y",y.shape, "y_g_hat",y_g_hat.shape)
+            #print("y",y.shape, "y_g_hat",y_g_hat.shape)
             y_df_hat_r, y_df_hat_g, fmap_f_r, fmap_f_g = mpd(y, y_g_hat)
             y_ds_hat_r, y_ds_hat_g, fmap_s_r, fmap_s_g = msd(y, y_g_hat)
             loss_fm_f = feature_loss(fmap_f_r, fmap_f_g)
@@ -195,7 +195,7 @@ def train(rank, a, h):
                     sw.add_scalar("training/mel_spec_error", mel_error, steps)
 
                 #Validation
-                if steps % a.validation_interval == 0:  # and steps != 0:
+                if steps % a.validation_interval == 0 and steps != 0:
                     generator.eval()
                     torch.cuda.empty_cache()
                     val_err_tot = 0
